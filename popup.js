5d92aab03b01be63e23ca632516cbaeffd657142
边宇琨 <bianyukun1213@outlook.com>
2020-03-09 21:52:29 +0800

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,93 @@
+var bg = chrome.extension.getBackgroundPage();
+var isCurrentTabVVClass;
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('name').innerText = '亮眼助手 ' + bg.getVer();
+    updateStatus();
+    document.getElementById('enableBtn').addEventListener('click', function () {
+        var input = prompt('启用口令：');
+        if (input == null)
+            return;
+        if (bg.checkPwd(input)) {
+            bg.enable();
+            if (isCurrentTabVVClass) {
+                alert('亮眼助手已启用！\n点击「确定」刷新以使更改生效。');
+                chrome.tabs.reload({ 'bypassCache': true });
+            } else
+                alert('亮眼助手已启用！\n若您已打开房间页面，请刷新以使更改生效。');
+            updateStatus();
+        } else
+            alert('口令错误！');
+    });
+    document.getElementById('disableBtn').addEventListener('click', function () {
+        bg.disable();
+        if (isCurrentTabVVClass) {
+            alert('亮眼助手已停用！\n点击「确定」刷新以使更改生效。');
+            chrome.tabs.reload({ bypassCache: true });
+        } else
+            alert('亮眼助手已停用！\n若您已打开房间页面，请刷新以使更改生效。');
+        updateStatus();
+    });
+    document.getElementById('roleChangeBtn').addEventListener('click', function () {
+        var input = prompt('请谨慎使用！\n角色代码（教师：teacher、助教：tutor、巡课人员或管理员：admin、互动学生：student、旁听学生：visitor）：');
+        var code = 'window.sessionStorage.setItem(\'vvroom.shinevv.role\',\'ROLE\');';
+        if (input == null)
+            return;
+        if (input == 'teacher' || input == 'tutor' || input == 'admin' || input == 'student' || input == 'visitor') {
+            chrome.tabs.executeScript({ code: code.replace('ROLE', input) }, function () {
+                alert('角色已变更！\n点击「确定」刷新以使更改生效。');
+                chrome.tabs.reload({ bypassCache: true });
+            });
+            updateStatus();
+
+        } else
+            alert('无效的角色代码！');
+    });
+    document.getElementById('roomChangeBtn').addEventListener('click', function () {
+        var input = prompt('请谨慎使用！\n房间号：');
+        var code = 'window.sessionStorage.setItem(\'vvroom.shinevv.roomId\',\'ROOM\');window.sessionStorage.setItem(\'vvroom.shinevv.roomTitle\',\'房间\');';
+        var reg = new RegExp('^[1-9][0-9]{0,4}$');
+        if (input == null)
+            return;
+        if (reg.test(input)) {
+            chrome.tabs.executeScript({ code: code.replace('ROOM', input) }, function () {
+                alert('房间已切换！\n点击「确定」刷新以使更改生效。');
+                chrome.tabs.reload({ bypassCache: true });
+            });
+            updateStatus();
+        } else
+            alert('无效的房间号！');
+    });
+});
+function updateStatus() {
+    if (bg.checkStatus()) {
+        document.getElementById('status').innerText = '状态：已启用';
+        document.getElementById('enableBtn').disabled = 'disabled';
+        document.getElementById('disableBtn').removeAttribute('disabled');
+    }
+
+    else {
+        document.getElementById('status').innerText = '状态：已停用';
+        document.getElementById('disableBtn').disabled = 'disabled';
+        document.getElementById('enableBtn').removeAttribute('disabled');
+    }
+    chrome.tabs.query({
+        currentWindow: true,
+        active: true
+    }, function (tab) {
+        if (tab[0].url.startsWith('https://vvclass.shinevv.com/?s=#/room')) {
+            isCurrentTabVVClass = true;
+            if (bg.checkStatus()) {
+                document.getElementById('roleChangeBtn').removeAttribute('disabled');
+                document.getElementById('roomChangeBtn').removeAttribute('disabled');
+            } else {
+                document.getElementById('roleChangeBtn').disabled = 'disabled';
+                document.getElementById('roomChangeBtn').disabled = 'disabled';
+            }
+        }
+        else {
+            isCurrentTabVVClass = false;
+            document.getElementById('roleChangeBtn').disabled = 'disabled';
+            document.getElementById('roomChangeBtn').disabled = 'disabled';
+        }
+    });
+}
